2df6c286607c85e376d614be7f6dae3803b42f28
true-pine <true_pine5@naver.com>
2023-03-23 00:13:22 +0900

    통과 풀이

--- 코딩테스트 입문/분수의 덧셈.java
+++ 코딩테스트 입문/분수의 덧셈.java
@@ -2,30 +2,31 @@ class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = {};
         
-        if(denom1 > denom2) {
-            if(denom1 % denom2 == 0) {
-                int multi = denom1 / denom2;
-                int numer = numer1 + (numer2 * multi);
-                int denom = denom1;
-                answer = new int[]{numer, denom};
-            } else {
-                int numer = (numer1 * denom2) + (numer2 * denom1);
-                int denom = denom1 * denom2;
-                answer = new int[]{numer, denom};
-            }
+        int numer = (numer1 * denom2) + (numer2 * denom1);
+        int denom = denom1 * denom2;
+        
+        int gcd = 0;
+        if(numer > denom) {
+            gcd = gcd(numer, denom);
         } else {
-            if(denom2 % denom1 == 0) {
-                int multi = denom2 / denom1;
-                int numer = (numer1 * multi) + numer2;
-                int denom = denom2;
-                answer = new int[]{numer, denom};
-            } else {
-                int numer = (numer1 * denom2) + (numer2 * denom1);
-                int denom = denom1 * denom2;
-                answer = new int[]{numer, denom};
-            }
+            gcd = gcd(denom, numer);
         }
+        
+        numer = numer / gcd;
+        denom = denom / gcd;
+        
+        answer = new int[]{numer, denom};
 
         return answer;
+    }
+    
+    private int gcd(int b, int s) {
+        while(s != 0) {
+            int tmp = b % s;
+            b = s;
+            s = tmp;
+        }
+        
+        return b;
     }
 }
